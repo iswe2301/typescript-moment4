edfcfd8e5dc1c69d2e73b0ba4d4a1d2f8e371446
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-16 16:16:29 +0200

    Skapat en metod för att hämta kurser

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,10 @@ export class CourseService {
   // Konstruktor
   constructor(private http: HttpClient) { } // Läser in http-klienten för att få tillgång i klassen
 
+  // Metod för att hämta kurser, tar emot en array av kurser enligt interfacet
+  getCourses(): Observable<Course[]> {
+
+    // Returnerar datat från URL:en genom get-metoden
+    return this.http.get<Course[]>(this.url);
+  }
 }
