1c6f7d76a823dea717c6853334358c9402d18edb
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-16 16:23:58 +0200

    Ändrat klass för coursecomponent: skapat egenskap för kurslista, skapat konstruktor med courseservice samt initmetod som hämtar kurserna.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'app-courses',
@@ -9,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+  // Egenskap för kurslista, av typen Course (interface) som är en array. Initerar en tom array.
+  courselist: Course[] = [];
+
+  // Konstruktor som importerar service för kurser
+  constructor(private courseservice: CourseService) { }
+
+  // Initmetod som körs när applikationen är startad och klar
+  ngOnInit() {
+    // Anropar metod för att hämta kurserna och prenumererar på svaret
+    this.courseservice.getCourses().subscribe(data => {
+      // Tar emot datan och lagrar i kurslistan (array)
+      this.courselist = data;
+    })
+  }
 }
