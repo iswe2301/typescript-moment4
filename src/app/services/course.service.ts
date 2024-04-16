12f3d0a20edc17d6db362d578d32babeb83f673c
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-16 16:11:26 +0200

    Skapat egenskap för url samt lagt till httpclient i konstruktorn.

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class CourseService {
 
-  constructor() { }
+  // Privat egenskap för url, av typen sträng
+  private url: string = "https://webbutveckling.miun.se/files/ramschema_ht23.json";
+
+  // Konstruktor
+  constructor(private http: HttpClient) { } // Läser in http-klienten för att få tillgång i klassen
+
 }
